1cae92864e47597ff28f374a2510cddee63c48d1
SanyaNanda <sanya.nanda@gmail.com>
2022-01-14 16:27:38 +0530

    Runtime: 7048 ms,Memory Usage: 20.1 MB

--- 6Companies30days/GoldmanSachs/1_Given_an_array_of_strings_ return_all_groups_of_strings_that_are_anagrams.py
+++ 6Companies30days/GoldmanSachs/1_Given_an_array_of_strings_ return_all_groups_of_strings_that_are_anagrams.py
@@ -1,5 +1,8 @@
 #---------Solved on Leetcode----------------------------------
 # python3
+# Runtime: 7048 ms, faster than 5.00% of Python3 online submissions for Group Anagrams.
+# Memory Usage: 20.1 MB, less than 9.74% of Python3 online submissions for Group Anagrams.
+
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         groups=[]
@@ -19,43 +22,3 @@ class Solution:
                     g.append(j[1])
             result.append(g)
         return result
-
-#---------Solved on GFG---------------------------------------
-# python3
-class Solution:
-    def Anagrams(self, words, n):
-        '''
-        words: list of word
-        return : list of group of anagram {list will be sorted in driver code (not word in grp)}
-        '''
-        groups=[]
-        for i in words:
-            group=sorted(list(i))
-            if group not in groups:
-                groups.append(group)
-                
-        str_to_group = [sorted(list(i)) for i in words]
-        group_map = list(zip(str_to_group,words))
-       
-        result=[]
-        for i in groups:
-            g=[]
-            for j in group_map:
-                if j[0]==i:
-                    g.append(j[1])
-            result.append(g)
-        return result
-#  Driver Code Starts
-if __name__ =='__main__':
-    t= int(input())
-    for tcs in range(t):
-        n= int(input())
-        words=input().split()
-        
-        ob = Solution()
-        ans = ob.Anagrams(words,n)
-        
-        for grp in sorted(ans):
-            for word in grp:
-                print(word,end=' ')
-            print()
