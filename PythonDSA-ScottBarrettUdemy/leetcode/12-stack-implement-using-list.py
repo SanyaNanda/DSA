819df5f36ced056e63553e8fe0a8c71156018603
SanyaNanda <sanya.nanda@gmail.com>
2024-10-22 16:00:55 +0530

    stack-queue notes and leetcode

---
+++ PythonDSA-ScottBarrettUdemy/leetcode/12-stack-implement-using-list.py
@@ -0,0 +1,80 @@
+class Stack:
+    def __init__(self):
+        self.stack_list = []
+
+    def print_stack(self):
+        for i in range(len(self.stack_list)-1, -1, -1):
+            print(self.stack_list[i])
+
+    def is_empty(self):
+        return len(self.stack_list) == 0
+
+    def peek(self):
+        if self.is_empty():
+            return None
+        else:
+            return self.stack_list[-1]
+
+    def size(self):
+        return len(self.stack_list)
+
+    def push(self, value):
+        self.stack_list.append(value)
+
+    def pop(self):
+        if self.is_empty():
+            return None
+        return self.stack_list.pop()
+ 
+            
+            
+            
+my_stack = Stack()
+print("Stack by using push:")
+my_stack.push(1)
+my_stack.push(2)
+my_stack.push(3)
+my_stack.print_stack()
+
+print("Stack before pop():")
+my_stack.print_stack()
+
+print("\nPopped node:")
+print(my_stack.pop())
+
+print("\nStack after pop():")
+my_stack.print_stack()
+
+print("\nPeeked node:")
+print(my_stack.peek())
+
+print("\nSize of the stack:")
+print(my_stack.size())
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Stack by using push:
+    3
+    2
+    1
+    Stack before pop():
+    3
+    2
+    1
+
+    Popped node:
+    3
+
+    Stack after pop():
+    2
+    1
+
+    Peeked node:
+    2
+
+    Size of the stack:
+    2
+ 
+"""
